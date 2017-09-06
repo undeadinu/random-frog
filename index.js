7a9773cd3702c960f8c42def87c9bf895ab390f2
srilq <git@srilq.email>
2017-09-06 22:51:25 +0100

    use xhr, standard

--- index.js
+++ index.js
@@ -1,50 +1,58 @@
-'use strict';
+'use strict'
 
-const uniqueRandomArray = require('unique-random-array');
-const randomCache = {};
+const uniqueRandomArray = require('unique-random-array')
+const xhr = require('request')
+const randomCache = {}
 
-function formatResult(getRandomImage) {
-    const imageData = getRandomImage();
-    if (!imageData) {
-        return;
-    }
-    return `https://imgur.com/${imageData.is_album ? imageData.album_cover : imageData.hash}${imageData.ext.replace(/\?.*/, '')}`;
+function formatResult (getRandomImage) {
+  const imageData = getRandomImage()
+  if (!imageData) {
+    return
+  }
+  return `https://imgur.com/${imageData.is_album ? imageData.album_cover : imageData.hash}${imageData.ext.replace(/\?.*/, '')}`
 }
 
-function storeResults(images, subreddit) {
-    const getRandomImage = uniqueRandomArray(images);
+function storeResults (images, subreddit) {
+  const getRandomImage = uniqueRandomArray(images)
 
-    randomCache[subreddit] = getRandomImage;
-    return getRandomImage;
+  randomCache[subreddit] = getRandomImage
+  return getRandomImage
 }
 
-function randomFrog(subreddit, cacheURL) {
-    subreddit = (typeof subreddit === 'string' && subreddit.length !== 0) ? subreddit : 'frogs';
+function randomFrog (subreddit, cacheURL) {
+  subreddit = (typeof subreddit === 'string' && subreddit.length !== 0) ? subreddit : 'frogs'
 
-    if (randomCache[subreddit]) {
-        return Promise.resolve(formatResult(randomCache[subreddit]));
-    }
+  if (randomCache[subreddit]) {
+    return Promise.resolve(formatResult(randomCache[subreddit]))
+  }
 
-    var url = typeof cacheURL === 'string' ? cacheURL : `https://imgur.com/r/${subreddit}/hot.json`;
-    return require('got')(url, {json: true})
-        .then(response => storeResults(response.body.data, subreddit))
-        .then(getRandomImage => formatResult(getRandomImage));
+  var url = typeof cacheURL === 'string' ? cacheURL : `https://imgur.com/r/${subreddit}/hot.json`
+
+  var p = new Promise(function (resolve, reject) {
+    xhr.get(url, {json: true}, function (err, resp, body) {
+      if (err) reject(err)
+      resolve(body.data)
+    })
+  })
+  return p.then(data => storeResults(data, subreddit))
+        .then(getRandomImage => formatResult(getRandomImage))
+        .catch(err => { throw err })
 }
 
-function callback(subreddit, cb, cacheURL) {
-    randomFrog(subreddit, cacheURL)
+function callback (subreddit, cb, cacheURL) {
+  randomFrog(subreddit, cacheURL)
         .then(url => cb(null, url))
-        .catch(err => cb(err));
+        .catch(err => cb(err))
 }
 
 // subreddit is optional
 // callback support is provided for a training exercise
-module.exports = (subreddit, cb, cacheURL) => {
-    if (typeof cb === 'function') {
-        callback(subreddit, cb, cacheURL);
-    } else if (typeof subreddit === 'function') {
-        callback(null, subreddit, cacheURL);
-    } else {
-        return randomFrog(subreddit, cacheURL);
-    }
-};
+module.exports = function (subreddit, cb, cacheURL) {
+  if (typeof cb === 'function') {
+    callback(subreddit, cb, cacheURL)
+  } else if (typeof subreddit === 'function') {
+    callback(null, subreddit, cacheURL)
+  } else {
+    return randomFrog(subreddit, cacheURL)
+  }
+}
